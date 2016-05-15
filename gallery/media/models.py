8610b96b9e28e694572248efcb39ecc4f9c0777d
Ville Korhonen <ville@xd.fi>
2016-05-15 17:03:23 +0000

    Add status to Collection

--- gallery/media/models.py
+++ gallery/media/models.py
@@ -3,11 +3,24 @@ from django.db import models
 
 from django_extensions.db.fields import CreationDateTimeField, ModificationDateTimeField, UUIDField
 
+_ = lambda x: x
+
+STATUS_ACTIVE = 'a'
+STATUS_INACTIVE = 'i'
+STATUS_REMOVED = 'r'
+
+STATUSES = (
+    (STATUS_ACTIVE, _('Active')),
+    (STATUS_INACTIVE, _('Inactive')),
+    (STATUS_REMOVED, _('Removed')),
+)
+
 class Collection(models.Model):
     uuid = UUIDField()
     owner = models.ForeignKey(settings.AUTH_USER_MODEL)
 
     title = models.CharField(max_length=255)
+    status = models.CharField(max_length=1, choices=STATUSES, default=STATUS_ACTIVE)
 
     created = CreationDateTimeField()
     modified = ModificationDateTimeField()
