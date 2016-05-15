b4dc9c59f1f57c4ba388777ddc8dab1bb56ce6a2
Ville Korhonen <ville@xd.fi>
2016-05-15 16:58:50 +0000

    Add initial Collection model

--- gallery/media/models.py
+++ gallery/media/models.py
@@ -1,3 +1,8 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+from django_extensions.db.fields import CreationDateTimeField, ModificationDateTimeField, UUIDField
+
+class Collection(models.Model):
+    uuid = UUIDField()
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL)
