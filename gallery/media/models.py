2016f360b57deb846dcb2ace01b1d2a921717daa
Ville Korhonen <ville@xd.fi>
2016-05-16 10:07:33 +0000

    Add users to Share

--- gallery/media/models.py
+++ gallery/media/models.py
@@ -95,6 +95,7 @@ class Share(models.Model):
     status = models.CharField(max_length=1, choices=STATUSES, default=STATUS_ACTIVE)
     share_type = models.CharField(max_length=1, choices=SHARE_TYPES, default=SHARE_WITH_LINK)
     password = models.CharField(max_length=255, blank=True, null=True)
+    users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='users')
 
     created = CreationDateTimeField()
     modified = ModificationDateTimeField()
